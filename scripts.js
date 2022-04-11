0acf94d21d1b4aac9c3fdb15b79000a8badc2d66
Pachelli <pachelli_moraes@hotmail.com>
2022-04-11 11:29:24 -0300

    Imagens nos cards

--- scripts.js
+++ scripts.js
@@ -19,31 +19,40 @@ function atualizaCard() {
         let acessarCards = document.querySelector(".cards");         
         acessarCards.innerHTML = "";
         for (let i = 0; i < comecar; i++) {       
-            if (cartas[i].status === "nao-virado") {               
+            if (cartas[i].status === "nao-virado") {              
                 acessarCards.innerHTML += `<div class="cardzin" onclick="virarCarta(this)"><!--${i}--><img src="front.png" alt="Parrot"></div>`;                
             } else {               
-                acessarCards.innerHTML += `<div class="cardzin virado" onclick="virarCarta(this)"><!--${i}--><img src="front.png" alt="Parrot"></div>`;
+                acessarCards.innerHTML += `<div class="cardzin virado" onclick="virarCarta(this)"><!--${i}--><img src="${cartas[i].imag}.gif" alt="Parrot"></div>`;
             }                 
         }     
         contadorJogadas();
 }
 
 
-function adicionaCards() {
+function adicionaCards() {   
+    let controlaImagem = 0; 
     for (let i = 0; i < comecar; i++) {
+        if (i % 2 == 0) {
+            controlaImagem += 1;
+        }
         const cartasAdicionadas = {
             status: "nao-virado",
+            imag: `${controlaImagem}`
             };
         cartas.push(cartasAdicionadas);
-    }        
+    }  
     atualizaCard();
-}
+}    
+    
 
 function virarCarta(elemento) {
+    let numeracao = 0;
     for (let i = 0; i < comecar; i++) {
         if (elemento.innerHTML === `<!--${i}--><img src="front.png" alt="Parrot">`) {
+            numeracao = cartas[i].imag;
             cartas[i] = {
                 status: "virado",
+                imag: numeracao              
             };
         }
     }
@@ -51,7 +60,7 @@ function virarCarta(elemento) {
 }
 
 function contadorJogadas() {
-    somaDeCartas.push("1");  
+    somaDeCartas.push("1");
     let soma = 0;
     for (let i = 0; i < comecar; i++) {       
         if (cartas[i].status === "virado") {               
@@ -60,7 +69,9 @@ function contadorJogadas() {
     if (soma === cartas.length) {
         alert(`O jogo acabou! Voce jogou ${somaDeCartas.length - 1} vezes`)
     }    
-}}
+}
+}
+
 
 
 
